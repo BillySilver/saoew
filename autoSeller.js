54da61ba8941460552118468ae94754fc1c9e501
BillySilver <as89366@yahoo.com.tw>
2016-11-08 22:37:23 +0800

    161108: A fix for SAOEW new chapter.

--- autoSeller.js
+++ autoSeller.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name        SAOEW : Auto Seller
 // @namespace   saoew
-// @include     http://a57528.app.gree-pf.net/sp_web.php?action=home_info_present_sell&guid=ON&c_key=*&step=2
+// @include     http://a57528.app.gree-pf.net/sp_web.php?action=home_info_present_sell&guid=ON&c_key=*&step=2*
+// @include     http://a57528.app.gree-pf.net/sp_web.php?guid=ON&action_home_info_present_sell=true&presentTable=1&opensocial_owner_id=*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
-// @version     [161006]
+// @version     [161108]
 // @grant       none
 // ==/UserScript==
 
