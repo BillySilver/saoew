48986d61b76f14e195c18caa52e5d8adfa911f09
BillySilver <BillySilvertw@gmail.com>
2016-12-08 21:28:50 +0800

    161207: 1. Some fixes for SAOEW new chapter. 2. A fix of new version "td.attack" since 160301.

--- autoSupportRequest.js
+++ autoSupportRequest.js
@@ -11,19 +11,18 @@
 // @include     http://a57528.app.gree-pf.net/sp_web.php?action_home_quest_detail_result=true&guid=ON&*
 // @include     http://a57528.app.gree-pf.net/sp_web.php
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
-// @version     [160707]
+// @version     [161207]
 // @grant       none
 // ==/UserScript==
 
 // var DEBUGGING = true;
-var opensocial_owner_id = 708131429;
 
 var nEnemyMinLv = 500;
 
 //*
 var isMobWhitelist = true;
 var mobWhitelist = [
-    "ｻﾞ･ﾄﾞﾚｯﾄﾞ･ｽｶｯﾀｰ"
+    "ｺﾎﾞﾙﾄﾞ･ｻﾞ･ｸﾞﾚｲﾄｿﾙｼﾞｬｰ"
 ];
 //*/
 
@@ -35,28 +34,29 @@ $(document).ready(function() {
 
     console.log("Support Request page.");
 
-    var isInIndexPage = isExisted("div#gad_wrapper>div>div>div#h_result_btn");
+    var isInIndexPage = isExisted("div#gad_wrapper > div > div > a > div.event_rank");
     var isFighting    = chkURL(/action_home_quest_detail_game/);
 
-    if ( isExisted("div#gad_wrapper>div>div>div.ask_btn") ) {
+    if ( isExisted("div#gad_wrapper > div > div > div.btn_request") ) {
         console.log("Index with some requests.");
 
-        $("div.ask_btn>a")[0].click();
+        $("div.btn_request > a")[0].click();
     } else if ( isInIndexPage ) {
         console.log("Index without request.");
 
         if ( confirm("Go to Invitation Page?") )
-            location.href = "sp_web.php?guid=ON&action_home_quest_accept_index=1&opensocial_owner_id=" + opensocial_owner_id;
-    } else if ( isExisted("div#gad_wrapper>div>div>center>div.gra_dark_blue") && isExisted("div#gad_wrapper>div>div>section.pager4>div.next") ) {
+            location.href = "sp_web.php?guid=ON&action_home_quest_accept_index=1&opensocial_owner_id=0";
+    // BUG: If there is only one page in Invitation Page, then it will do nothing!
+    } else if ( isExisted("div#gad_wrapper > div > div > center > div.gra_dark_blue") && isExisted("div#gad_wrapper > div > div > section.pager4 > div.next") ) {
         console.log("Finding requests...");
 
         var nBestRequest = 0;
         var arrEnemyData = [[0, 0]];
-        for (var i = 0; i < $("div.gra_dark_blue td>span>span:first-child").length; i++) {
-            var nEnemyLv  = parseInt($("div.gra_dark_blue td>span>span:first-child").eq(i).html().match(/Lv\d{1,3}/)[0].replace("Lv", ""));
+        for (var i = 0; i < $("div.gra_dark_blue td > span > span:first-child").length; i++) {
+            var nEnemyLv  = parseInt($("div.gra_dark_blue td > span > span:first-child").eq(i).html().match(/Lv\d{1,3}/)[0].replace("Lv", ""));
             var nLeftTime = (function() {
-                if ( null !== $("div.gra_dark_blue td>span").eq(i).html().match(/\d:\d\d:\d\d/) ) {
-                    var arrLeftTime = $("div.gra_dark_blue td>span").eq(i).html().match(/\d:\d\d:\d\d/)[0].split(":");
+                if ( null !== $("div.gra_dark_blue td > span").eq(i).html().match(/\d:\d\d:\d\d/) ) {
+                    var arrLeftTime = $("div.gra_dark_blue td > span").eq(i).html().match(/\d:\d\d:\d\d/)[0].split(":");
                     return parseInt(arrLeftTime[0])*3600 + parseInt(arrLeftTime[1])*60 + parseInt(arrLeftTime[2]);
                 } else {
                     return 0;
@@ -66,7 +66,7 @@ $(document).ready(function() {
             var isInMobWhitelist = (function() {
                 if ( true === isMobWhitelist ) {
                     console.log("Mob Name: ");
-                    var strMobName = $("div.gra_dark_blue td>span").eq(i).html().match(/[^>]+\[Lv/)[0].replace("[Lv", "");
+                    var strMobName = $("div.gra_dark_blue td > span").eq(i).html().match(/[^>]+\[Lv/)[0].replace("[Lv", "");
                     console.log("\t" + strMobName);
 
                     if ( -1 === mobWhitelist.indexOf(strMobName) ) {
@@ -90,55 +90,55 @@ $(document).ready(function() {
         }
 
         if ( 0 !== nBestRequest ) {
-            $("div.gra_dark_blue>div.block_bt_r>a")[nBestRequest-1].click();
+            $("div.gra_dark_blue > div.block_bt_r > a")[nBestRequest-1].click();
         }
     // ともに闘う!!
     // http://a57528.app.gree-pf.net/sp_web.php?guid=ON&action_home_quest_accept_detail=1&qid=*
     // 可能會和其他表單按鈕衝突!
-    // } else if ( chkURL(/action_home_quest_accept_detail/) && isExisted("div#gad_wrapper>div:eq(3)>form:eq(0)>div:eq(3)>center.padding2.btn01>input") ) {
-    } else if ( chkURL(/action_home_quest_accept_detail/) && isExisted("div#gad_wrapper>div:eq(4)>form:eq(0)>div:eq(3)>center.padding2.btn01>input") ) {
+    // } else if ( chkURL(/action_home_quest_accept_detail/) && isExisted("div#gad_wrapper > div:eq(3) > form:eq(0) > div:eq(3) > center.padding2.btn01 > input") ) {
+    } else if ( chkURL(/action_home_quest_accept_detail/) && isExisted("div#gad_wrapper > div:eq(4) > form:eq(0) > div:eq(3) > center.padding2.btn01 > input") ) {
         console.log("Attend!");
 
-        $("center.padding2.btn01>input")[0].click();
+        $("center.padding2.btn01 > input")[0].click();
     // この協力ﾊﾞﾄﾙには参加できません
     // http://a57528.app.gree-pf.net/sp_web.php?guid=ON&action_home_quest_accept_detail=1&qid=*
     // 可能會和其他表單按鈕衝突!
-    } else if ( chkURL(/action_home_quest_accept_detail/) && isExisted("div#gad_wrapper>div>center.footer_padding>p.footer_btn>a") ) {
+    } else if ( chkURL(/action_home_quest_accept_detail/) && isExisted("div#gad_wrapper > div > center.footer_padding > p.footer_btn > a") ) {
         console.log("Back to Requests!");
 
-        $("p.footer_btn>a")[0].click();
+        $("p.footer_btn > a")[0].click();
     // さっそく戦う!
     // http://a57528.app.gree-pf.net/sp_web.php
-    } else if ( chkURL(/^$/) && isExisted("div#gad_wrapper>div>div.clear_black>center.padding2.btn01>a") ) {
+    } else if ( chkURL(/^$/) && isExisted("div#gad_wrapper > div > div.clear_black > center.padding2.btn01 > a") ) {
         console.log("Go!");
 
-        $("center.padding2.btn01>a")[0].click();
+        $("center.padding2.btn01 > a")[0].click();
     // Fighting (First).
     // http://a57528.app.gree-pf.net/sp_web.php?action_home_quest_detail_index=true&guid=ON&opensocial_owner_id=*
-    } else if ( chkURL(/action_home_quest_detail_index/) && isExisted("div#gad_wrapper>div>table>tbody>tr>td.attack") ) {
+    } else if ( chkURL(/action_home_quest_detail_index/) && isExisted("div#gad_wrapper > div > table > tbody > tr > td.attack") ) {
         console.log("Without waiting. Ignore HP of the enemy.");
 
         // if AP is not enough, "input[name=isConfirmedUseItem]" will display.
         // $("td.attack > div > form > div.margin_t2 > input.quest_heal_btn") is always existed since Burst Skill (Battle Skill) System.
         if ( false === isExisted("input[name=isConfirmedUseItem]") ) {
-            $("td.attack > a")[0].click();
+            $("td.attack > div > form > div.margin_t2 > input.quest_heal_btn")[0].click();
         } else {
             console.log("AP is not enough.");
         }
     // After Fighting. (in SAOEW : Auto Event)
     // http://a57528.app.gree-pf.net/sp_web.php?action=home_quest_detail_attack&tu=*
-    } else if ( isExisted("div#gad_wrapper>div>table>tbody>tr>td.attack") ) {
+    } else if ( isExisted("div#gad_wrapper > div > table > tbody > tr > td.attack") ) {
         // do nothing.
     // Next Fight: Fight Finish or Timeout. (in SAOEW : Auto Event)
     // The Battle may end when Fighting. (action=home_quest_detail_attack)
-    } else if ( isExisted("div#gad_wrapper>div>div>div>center>div.next_bt") ) {
-        // $("div.next_bt>a")[0].click();
+    } else if ( isExisted("div#gad_wrapper > div > div > div > center > div.next_bt") ) {
+        // $("div.next_bt > a")[0].click();
 
         // If the fight is Timeout, then div.next_bt will go to action_home_quest_index and do nothing.
-        location.href = "sp_web.php?guid=ON&action_home_quest_accept_index=1&opensocial_owner_id=" + opensocial_owner_id;
+        location.href = "sp_web.php?guid=ON&action_home_quest_accept_index=1&opensocial_owner_id=0";
     // Skip Battle Result.
     // 現在、結果集計処理中です。
-    // http://a57528.app.gree-pf.net/sp_web.php?guid=ON&action_home_quest_accept_index=1&opensocial_owner_id=708131429
+    // http://a57528.app.gree-pf.net/sp_web.php?guid=ON&action_home_quest_accept_index=1&opensocial_owner_id=*
     } else if ( chkURL(/action(_|=)home_quest_detail_attack/) || chkURL(/action(_|=)home_quest_detail_index/) || chkURL(/action_home_quest_accept_index/) ) {
         // Form Offical Code.
 
@@ -158,8 +158,10 @@ $(document).ready(function() {
             console.log("*** Debugging Mode ***");
     }
 
-    if (
-        (false === chkURL(/^$/) || isExisted("div#gad_wrapper>div>div.clear_black>center.padding2.btn01>a")) &&
+    if ( false === DEBUGGING &&
+        (false === chkURL(/^$/) ||
+            isExisted("div#gad_wrapper > div > div.clear_black > center.padding2.btn01 > a") ||
+            isExisted("div#gad_wrapper > div > table > tbody > tr > td.attack") ) &&
         false === isInIndexPage &&
         false === isFighting
     ) {
