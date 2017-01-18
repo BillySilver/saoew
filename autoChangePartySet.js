e08c2d992c8e3239cbb719e89e8e9142ebe9e3a0
BillySilver <BillySilvertw@gmail.com>
2017-01-18 23:59:01 +0800

    170113: New feature: Auto Change Party Set (used with autoEvent.js or any feature which changes Party Set).

---
+++ autoChangePartySet.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name        SAOEW : Auto Change Party Set
+// @namespace   saoew
+// @include     http://a57528.app.gree-pf.net/sp_web.php?guid=ON&action_home_info_formation_setlist2=true&opensocial_owner_id=*
+// @include     http://a57528.app.gree-pf.net/sp_web.php?action_home_info_formation_setlist2=true&guid=ON&opensocial_owner_id=*
+// @include     http://a57528.app.gree-pf.net/sp_web.php?action=home_info_formation_setlist2&guid=ON
+// @include     http://a57528.app.gree-pf.net/sp_web.php
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+// @version     [170113]
+// @grant       none
+// ==/UserScript==
+
+var nTrainingSet = 6;
+
+var CSS = {
+    mode: "color: crimson; font-weight: bold;",
+    info: "color: blueviolet; font-weight: bold;",
+    err:  "color: red; font-weight: bold;",
+};
+
+$(document).ready(function() {
+    if ( false === isExisted("div#gad_wrapper > div > div.margin_t2 > div > form > select[name=deckCode]") )
+        return;
+
+    if ( chkURL(/^$/) ) {
+        // Go back to Formation Page without form data sending.
+        hAfterWaiting();
+    }
+
+    var nSelectedSet = $("select[name=deckCode] > option:selected").text().match(/\d+/)[0].toInt();
+    console.log(nSelectedSet);
+    if ( nTrainingSet !== nSelectedSet ) {
+        setTimeout(function() {
+            $("select[name=deckCode] > option").eq(nTrainingSet - 1).prop("selected", true);
+            $("select[name=deckCode]").next("div.btn02").children("input")[0].click();
+        }, 5*1000);
+    }
+
+    setTimeout(hAfterWaiting, 90*1000);
+
+    function hAfterWaiting() {
+        console.log("Time Out!");
+        $("li.others.pattern2.btn > a")[0].click();
+        $("div.gl_btn.formation > a")[0].click();
+    }
+});
+
+/**
+ * For general use.
+ */
+
+function isExisted(strSelector) {
+    if (0 !== $(strSelector).length) {
+        console.log("Selector Found: %c%s", CSS.info, strSelector);
+        return true;
+    }
+    else return false;
+}
+
+function chkURL(regexURL) {
+    if (null !== location.search.match(regexURL)) {
+        console.log("URL Found:", regexURL);
+        return true;
+    }
+    else return false;
+}
+
+String.prototype.toInt = function() {
+    return parseInt(this);
+};
